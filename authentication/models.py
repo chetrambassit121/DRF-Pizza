857408554eb88b9abb860170554de8c3a32173f9
Chetram Bassit <chetrambassit121@gmail.com>
2022-06-26 14:07:45 -0400

    CustomUserManager class created in models.py

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,42 @@
 from django.db import models
-
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import AbstractUser
+# from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self,email,password,**extra_fields):
+        if not email:
+            raise ValueError(_('Please enter an email address'))
+
+        email=self.normalize_email(email)
+
+        new_user=self.model(email=email,**extra_fields)
+
+        new_user.set_password(password)
+
+        new_user.save()
+
+        return new_user
+
+
+    def create_superuser(self,email,password,**extra_fields):
+
+        extra_fields.setdefault('is_superuser',True)
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_active',True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True'))
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True'))
+
+
+        return self.create_user(email,password,**extra_fields)
+
+
+
+
