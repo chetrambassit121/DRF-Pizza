9692b04f595c549302467e8ed7a7ae5139768509
Chetram Bassit <chetrambassit121@gmail.com>
2022-06-26 13:19:13 -0400

    created authentication and orders apps

--- pizza/settings.py
+++ pizza/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'authentication'
+    'authentication.apps.AuthenticationConfig',
+    'orders.apps.OrdersConfig'
+
 ]
 
 MIDDLEWARE = [
