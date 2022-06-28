7b8d9e516fc2d898e613e19d460ff15a2b64c266
Chetram Bassit <chetrambassit121@gmail.com>
2022-06-28 12:24:32 -0400

    ordercreatelistview defined the get method

--- orders/views.py
+++ orders/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
+from . import serializers
+from .models import Order
 
 class HelloOrderView(generics.GenericAPIView):
     def get(self,request):
         return Response(data={"message":"Hello Orders"}, status=status.HTTP_200_OK)
 
 class OrderCreateListView(generics.GenericAPIView):
+    serializer_class = serializers.OrderCreatiobSerializer
 
     def get(self, request):
-        pass 
+        orders = Order.objects.all()
+        serializer = self.serializer_class(instance=orders)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
 
     def post(self, request):
         pass 
