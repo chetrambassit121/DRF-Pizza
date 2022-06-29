fea5799cd7fd63355d696358f93032d534f842c5
Chetram Bassit <chetrambassit121@gmail.com>
2022-06-28 22:38:25 -0400

    heroku deployent

--- pizza/settings.py
+++ pizza/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 from decouple import config 
 from datetime import timedelta
 import dj_database_url
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -172,3 +173,4 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_on_heroku.settings(locals()) 
